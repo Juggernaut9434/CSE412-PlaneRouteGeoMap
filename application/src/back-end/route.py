64ac79ecdf2ad7ffcb2dfd3cc993695fc1df5785
Jordan Perry <jordanperry@centurylink.net>
2022-11-21 19:34:37 -0700

    Added Mike's SQL to improve initialization time

--- application/src/back-end/route.py
+++ application/src/back-end/route.py
@@ -22,3 +22,7 @@ class Route():
     def getDestination(self):
         """Returns destination tuple (latitude, longitude) for mapping"""
         return (self.dest_lat, self.dest_long)
+
+    def __str__(self) -> str:
+        """Sets string representation of Route object to display it's information"""
+        return f"Route object: [Airline: {self.airline}, Source: ({self.src_iata}, {self.src_lat}, {self.src_long}), Destination: ({self.dest_iata}, {self.dest_lat}, {self.dest_long})]"
